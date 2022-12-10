20d1ccb960044dab7b24d4c3742d22a9d5a687c3
cho196 <91933101+cho196@users.noreply.github.com>
2022-12-10 00:17:10 -0800

    fix 3485789

--- next.config.js
+++ next.config.js
@@ -5,15 +5,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-module.exports = {
-  // …
-  async rewrites() {
-    return [
-      {
-        source: "/index",
-        destination: "/indexPage",
-      },
-    ];
-  },
-  // …
-};
--- pages/moodpicker.js
+++ pages/moodpicker.js
@@ -66,7 +66,7 @@ export default function MoodPicker() {
       <Content>
         <StyledButton
           onClick={() => {
-            r.push("/indexPage");
+            r.push("/");
           }}
         >
           Back To Home
