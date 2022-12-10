90899f59a52c7822154428de97f4472f381c233a
cho196 <91933101+cho196@users.noreply.github.com>
2022-12-10 00:08:51 -0800

    fix

--- pages/moodpicker.js
+++ pages/moodpicker.js
@@ -66,7 +66,7 @@ export default function MoodPicker() {
       <Content>
         <StyledButton
           onClick={() => {
-            r.push("/");
+            r.push("/indexPage");
           }}
         >
           Back To Home
