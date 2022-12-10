322bbcf22889de79c378e797bb96f2d22284b132
cho196 <91933101+cho196@users.noreply.github.com>
2022-12-10 00:02:07 -0800

    fix index

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+module.exports = {
+  // …
+  async rewrites() {
+    return [
+      {
+        source: "/index",
+        destination: "/indexPage",
+      },
+    ];
+  },
+  // …
+};
